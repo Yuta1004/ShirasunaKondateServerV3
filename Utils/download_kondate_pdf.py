62af23ad0e1ab3861b82c2b5c0562da8c36dc568
NakagamiYuta <guguru0014@gmail.com>
2019-04-13 13:44:50 +0900

    [fix]献立ページ新仕様対応

--- Utils/download_kondate_pdf.py
+++ Utils/download_kondate_pdf.py
@@ -6,7 +6,7 @@ from Utils.format import christian_to_japanese
 
 def download_kondate_pdf(year, month):
     url = "http://shirasunaryou.sakura.ne.jp/cgi-bin/shirasuna/kondate/"
-    url += christian_to_japanese(year, month) + "/" + str(month).zfill(2) + ".pdf"
+    url += str(year) + "/" + str(month).zfill(2) + ".pdf"
     download_path = os.path.dirname(os.path.abspath(__file__).replace("/Utils", "")) + "/PDFData/"
 
     try:
